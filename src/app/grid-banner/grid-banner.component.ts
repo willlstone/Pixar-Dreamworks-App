fe0da8f0aa2afe098d60f2c37abd37aa3647acfe
willlstone <stonew2@rpi.edu>
2019-11-04 16:44:43 -0500

    Banner to change dynamically on production company, removed debugging console.logs, updated navbar locations

--- src/app/grid-banner/grid-banner.component.ts
+++ src/app/grid-banner/grid-banner.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
 
 @Component({
   selector: 'app-grid-banner',
@@ -6,10 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./grid-banner.component.scss']
 })
 export class GridBannerComponent implements OnInit {
-
-  constructor() { }
-
+  studio = +this.route.snapshot.paramMap.get('id');
+  bannerURL;
+  constructor(
+    private route: ActivatedRoute,
+    private location: Location
+  ) { }
+setBanner() {
+  switch (this.studio) {
+    case 3:
+      this.bannerURL = 'https://wallpaperaccess.com/full/331666.jpg';
+      break;
+    case 521:
+      this.bannerURL = 'https://images5.alphacoders.com/499/499486.jpg';
+      break;
+    default:
+      console.log(this.studio);
+      break;
+  }
+}
   ngOnInit() {
+    this.setBanner();
   }
 
 }
