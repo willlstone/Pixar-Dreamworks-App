54ee22008e35ac1119962786a1dca93de3ac8e46
ERIC PORTER <Eric.Porter01@libertymutual.com>
2019-11-08 12:19:04 -0500

    Made the dropdown more useful

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,10 +7,7 @@ import {MovieDetailComponent} from './movie-detail/movie-detail.component';
 import {BioComponent} from './bio/bio.component';
 
 const routes: Routes = [
-  // {path: 'studio/:id', component: StudioComponent },
-  {path: '', pathMatch: 'full', component: HomePageComponent},
   {path: 'home', redirectTo: '', component: HomePageComponent},
-  // {path: 'movie/:id', component: MovieDetailComponent },
   {path: 'person/:id', component: BioComponent },
   {path: 'details/:grid/:id', component: MovieDetailComponent },
   {path: ':grid/:id', component: StudioComponent },
